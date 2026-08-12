1e453151ddd85fa33157b9ce56fef06369e60164
IVAN <ArvanMarketer017@gmail.com>
2026-08-13 00:11:10 +0700

    Release v5.6 — Rilis besar -- lihat Catatan Perubahan untuk daftar fitur.

--- pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.2.5 (trial), 000000, 2026-07-05T10:18:35.058863
-from .pyarmor_runtime import __pyarmor__
---
+++ pyarmor_runtime_015293/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.2.5 (pro), 015293, 2026-08-12T12:22:14.572938
+from .pyarmor_runtime import __pyarmor__
